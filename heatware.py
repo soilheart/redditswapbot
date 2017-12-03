baa4cd6f1c00da6d241f1d6fad04a78196483ea0
Daniel Carlsson <danneck@gmail.com>
2017-12-03 19:44:39 +0100

    Heatware: Updated heatware to use SubRedditMod and added more features.

--- heatware.py
+++ heatware.py
@@ -1,68 +1,59 @@
 #!/usr/bin/env python2
 
-import sys, os
-from ConfigParser import SafeConfigParser
-import praw
 import re
-from datetime import datetime, timedelta
-from time import sleep, time
 from log_conf import LoggerManager
-
-# load config file
-containing_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
-cfg_file = SafeConfigParser()
-path_to_cfg = os.path.join(containing_dir, 'config.cfg')
-cfg_file.read(path_to_cfg)
-username = cfg_file.get('reddit', 'username')
-password = cfg_file.get('reddit', 'password')
-app_key = cfg_file.get('reddit', 'app_key')
-app_secret = cfg_file.get('reddit', 'app_secret')
-subreddit = cfg_file.get('reddit', 'subreddit')
-link_id = cfg_file.get('heatware', 'link_id')
-reply = cfg_file.get('heatware', 'reply')
-regex = cfg_file.get('heatware', 'regex')
+from common import SubRedditMod, DictConfigParser
 
 # Configure logging
 logger = LoggerManager().getLogger(__name__)
 
+
+def process_comment(subreddit, cfg, comment):
+    """ Process a heatware thread comment"""
+    logger.debug("Processing comment: " + comment.id)
+    if subreddit.check_mod_reply(comment):
+        # If a mod has already replied, case closed
+        return
+
+    heatware = re.search(cfg["regex"], comment.body)
+    if not heatware:
+        # If no match, notify user
+        comment.reply("No heatware link found, please double check your link and make a new comment")
+        return
+
+    new_flair = heatware.group(0)
+    if comment.author_flair_text:
+        # If user already have flair text set
+        if cfg["overwrite_flair"]:
+            subreddit.update_comment_user_flair(comment, text=new_flair)
+            if cfg["report_overwrite"]:
+                comment.report("Overwritten flair: %s" % comment.author_flair_text)
+        else:
+            if cfg["report_overwrite"]:
+                comment.report("User already has flair")
+        if cfg["overwrite_msg"]:
+            comment.reply(cfg["overwrite_msg"])
+    else:
+        subreddit.update_comment_user_flair(comment, text=new_flair)
+        if cfg["add_msg"]:
+            comment.reply(cfg["add_msg"])
+
+
+def process_thread(subreddit):
+    """ Get and process heatware thread comments """
+    cfg = subreddit.config["heatware"]
+    comments = subreddit.get_all_comments(cfg["link_id"])
+    for comment in comments:
+        if not hasattr(comment, 'author'):
+            continue
+        if comment.is_root is True:
+            process_comment(subreddit, cfg, comment)
+
+
 def main():
     try:
-        logger.debug('Logging in as /u/' + username)
-        r = praw.Reddit(client_id=app_key,
-                        client_secret=app_secret,
-                        username=username,
-                        password=password,
-                        user_agent=username)
-
-        # Get the submission and the comments
-        submission = r.submission(id=link_id)
-        submission.comments.replace_more(limit=None, threshold=0)
-        flat_comments = submission.comments.list()
-
-        for comment in flat_comments:
-            logger.debug("Processing comment: " + comment.id)
-            if not hasattr(comment, 'author'):
-                continue
-            if comment.is_root is True:
-                heatware = re.search(regex, comment.body)
-                if heatware:
-                    url = heatware.group(0)
-                    if not comment.author_flair_text:
-                        replies_flat = comment.replies.list()
-                        for reply in replies_flat:
-                            if reply.author:
-                                if str(reply.author.name) == username:
-                                    break
-                        else:
-                            if comment.author:
-                                if comment.author_flair_css_class:
-                                    r.subreddit(subreddit).flair.set(comment.author, url, comment.author_flair_css_class)
-                                else:
-                                    r.subreddit(subreddit).flair.set(comment.author, url, '')
-                                logger.info('Set ' + comment.author.name + '\'s heatware to ' + url)
-                                if reply:
-                                    comment.reply(reply)
-
+        subreddit = SubRedditMod(logger)
+        process_thread(subreddit)
     except Exception as e:
         logger.error(e)
 
