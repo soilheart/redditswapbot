5e3e79ed6773aa775bc88b2d41c8913a2a5a351a
Daniel Carlsson <danneck@gmail.com>
2018-04-29 00:18:56 +0200

    LoggerManager: Added sentry (sentry.io) support to logger

--- log_conf.py
+++ log_conf.py
@@ -9,6 +9,14 @@ cfg_file = SafeConfigParser()
 path_to_cfg = os.path.join(containing_dir, 'config.cfg')
 cfg_file.read(path_to_cfg)
 logging_dest = cfg_file.get('logging','dest')
+sentry = cfg_file.get('logging','sentry')
+
+try:
+    from raven.handlers.logging import SentryHandler
+    from raven.conf import setup_logging
+except ImportError:
+    # Raven not installed, skip sentry even though config exists
+    sentry = ""
 
 mysql_hostname = cfg_file.get('mysql', 'hostname')
 mysql_username = cfg_file.get('mysql', 'username')
@@ -16,38 +24,43 @@ mysql_password = cfg_file.get('mysql', 'password')
 mysql_database = cfg_file.get('mysql', 'database')
 
 class Singleton(type):
-	_instances = {}
+    _instances = {}
 
-	def __call__(cls, *args, **kwargs):
-		if cls not in cls._instances.keys():
-			cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-		return cls._instances[cls]
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances.keys():
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 class LoggerManager(object):
-	__metaclass__ = Singleton
+    __metaclass__ = Singleton
 
-	_loggers = {}
+    _loggers = {}
 
-	def __init__(self, *args, **kwargs):
-		pass
+    def __init__(self, *args, **kwargs):
+        pass
 
-	@staticmethod
-	def getLogger(name=None):
-		#configure logging
-		LoggerManager._loggers[name] = logging.getLogger(name)
-		LoggerManager._loggers[name].setLevel(logging.INFO)
-		
-		if logging_dest == 'mysql':
-			db = {'host':mysql_hostname, 'port':3306, 'dbuser':mysql_username, 'dbpassword':mysql_password, 'dbname':mysql_database}
-		
-			sqlh = mySQLHandler.mySQLHandler(db)
-			LoggerManager._loggers[name].addHandler(sqlh)
-		else:
-			fileh = logging.FileHandler('actions.log')
-			fileh.setFormatter(logging.Formatter('%(asctime)s - %(module)s - %(message)s'))
-			LoggerManager._loggers[name].addHandler(fileh)
-		
-		requests_log = logging.getLogger("requests")
-		requests_log.setLevel(logging.WARNING)
-		
-		return LoggerManager._loggers[name]
+    @staticmethod
+    def getLogger(name=None):
+        #configure logging
+        LoggerManager._loggers[name] = logging.getLogger(name)
+        LoggerManager._loggers[name].setLevel(logging.INFO)
+
+        if logging_dest == 'mysql':
+            db = {'host':mysql_hostname, 'port':3306, 'dbuser':mysql_username, 'dbpassword':mysql_password, 'dbname':mysql_database}
+
+            sqlh = mySQLHandler.mySQLHandler(db)
+            LoggerManager._loggers[name].addHandler(sqlh)
+        else:
+            fileh = logging.FileHandler('actions.log')
+            fileh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(module)s - %(message)s'))
+            LoggerManager._loggers[name].addHandler(fileh)
+
+        if sentry:
+            sentryh = SentryHandler(sentry)
+            sentryh.setLevel(logging.ERROR)
+            setup_logging(sentryh)
+
+        requests_log = logging.getLogger("requests")
+        requests_log.setLevel(logging.WARNING)
+
+        return LoggerManager._loggers[name]
